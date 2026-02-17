d37db2567c3dce9e97a8c3a81cb9980df740b802
ashenwattegederastu <2023is114@stu.ucsc.cmb.ac.lk>
2026-02-17 05:51:12 +0530

    aa

--- src/main/java/com/dailyfixer/util/DBConnection.java
+++ src/main/java/com/dailyfixer/util/DBConnection.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/dfguidestore"
+    private static final String URL = "jdbc:mysql://localhost:3306/df_testing"
             + "?useSSL=false"
             + "&serverTimezone=UTC"
             + "&allowPublicKeyRetrieval=true";
--- src/main/webapp/listguides.jsp
+++
@@ -1,2 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
-    <% // Redirect to the new guide list servlet response.sendRedirect(request.getContextPath() + "/guides" ); %>
